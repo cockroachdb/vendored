5587194a383f99633c510efa7414223828760be6
Raphael 'kena' Poss <knz@cockroachlabs.com>
2018-04-05 17:09:38 +0200

    Update libedit

--- github.com/knz/go-libedit/unix/editline_unix.go
+++ github.com/knz/go-libedit/unix/editline_unix.go
@@ -14,7 +14,7 @@ import (
 
 // #cgo openbsd netbsd freebsd dragonfly darwin LDFLAGS: -ledit
 // #cgo openbsd netbsd freebsd dragonfly darwin CPPFLAGS: -Ishim
-// #cgo linux LDFLAGS: -ltinfo
+// #cgo linux LDFLAGS: -lncurses
 // #cgo linux CFLAGS: -Wno-unused-result
 // #cgo linux CPPFLAGS: -Isrc -Isrc/c-libedit -Isrc/c-libedit/editline -Isrc/c-libedit/linux-build
 // #cgo darwin CPPFLAGS: -D__darwin__=1
