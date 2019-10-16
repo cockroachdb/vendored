7bfabbd687f38429932fc19ad209c3dd8afe7cd5
Andrew Kryczka <andrew.kryczka2@gmail.com>
2019-10-16 17:09:24 +0000

    bump pebble to 3ddb11543fe64e21f76524ea7beca673fac7a6cb

--- github.com/cockroachdb/pebble/compaction.go
+++ github.com/cockroachdb/pebble/compaction.go
@@ -51,7 +51,7 @@ func totalSize(f []fileMetadata) (size uint64) {
 type compaction struct {
 	cmp     Compare
 	format  base.Formatter
-	logger  base.Logger
+	logger  Logger
 	version *version
 
 	// startLevel is the level that is being compacted. Inputs from startLevel
@@ -635,7 +635,7 @@ func (d *DB) getCompactionPacerInfo() compactionPacerInfo {
 		totalCompactionDebt: d.mu.versions.picker.estimatedCompactionDebt(bytesFlushed),
 	}
 	for _, m := range d.mu.mem.queue {
-		pacerInfo.totalDirtyBytes += m.totalBytes()
+		pacerInfo.totalDirtyBytes += m.inuseBytes()
 	}
 	d.mu.Unlock()
 
@@ -646,7 +646,7 @@ func (d *DB) getFlushPacerInfo() flushPacerInfo {
 	var pacerInfo flushPacerInfo
 	d.mu.Lock()
 	for _, m := range d.mu.mem.queue {
-		pacerInfo.totalBytes += m.totalBytes()
+		pacerInfo.inuseBytes += m.inuseBytes()
 	}
 	d.mu.Unlock()
 	return pacerInfo
@@ -769,6 +769,7 @@ func (d *DB) flush1() error {
 		}
 	}
 
+	d.mu.versions.incrementFlushes()
 	d.opts.EventListener.FlushEnd(info)
 
 	// Refresh bytes flushed count.
@@ -897,6 +898,7 @@ func (d *DB) compact1() (err error) {
 			info.Output.Tables = append(info.Output.Tables, e.Meta.TableInfo())
 		}
 	}
+	d.mu.versions.incrementCompactions()
 	d.opts.EventListener.CompactionEnd(info)
 
 	// Update the read state before deleting obsolete files because the
@@ -927,7 +929,8 @@ func (d *DB) runCompaction(jobID int, c *compaction, pacer pacer) (
 		meta := &c.inputs[0][0]
 		c.metrics = map[int]*LevelMetrics{
 			c.outputLevel: &LevelMetrics{
-				BytesMoved: meta.Size,
+				BytesMoved:  meta.Size,
+				TablesMoved: 1,
 			},
 		}
 		ve := &versionEdit{
@@ -995,6 +998,8 @@ func (d *DB) runCompaction(jobID int, c *compaction, pacer pacer) (
 		c.outputLevel: metrics,
 	}
 
+	writerOpts := d.opts.MakeWriterOptions(c.outputLevel)
+
 	newOutput := func() error {
 		d.mu.Lock()
 		fileNum := d.mu.versions.getNextFileNum()
@@ -1022,7 +1027,7 @@ func (d *DB) runCompaction(jobID int, c *compaction, pacer pacer) (
 		})
 		filenames = append(filenames, filename)
 		cacheOpts := private.SSTableCacheOpts(d.cacheID, fileNum).(sstable.WriterOption)
-		tw = sstable.NewWriter(file, d.opts, d.opts.Level(c.outputLevel), cacheOpts)
+		tw = sstable.NewWriter(file, writerOpts, cacheOpts)
 
 		ve.NewFiles = append(ve.NewFiles, newFileEntry{
 			Level: c.outputLevel,
@@ -1068,6 +1073,11 @@ func (d *DB) runCompaction(jobID int, c *compaction, pacer pacer) (
 		meta.LargestSeqNum = writerMeta.LargestSeqNum
 
 		metrics.BytesWritten += meta.Size
+		if c.flushing == nil {
+			metrics.TablesCompacted++
+		} else {
+			metrics.TablesFlushed++
+		}
 
 		// The handling of range boundaries is a bit complicated.
 		if n := len(ve.NewFiles); n > 1 {
