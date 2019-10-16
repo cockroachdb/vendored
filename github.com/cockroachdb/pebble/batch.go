50a346235faf7125671f2ae97041edc9a9f43631
Andrew Kryczka <andrew.kryczka2@gmail.com>
2019-10-16 17:09:24 +0000

    bump pebble to 9d80716063bb38e2772ced37c428ccc47d384b23

--- github.com/cockroachdb/pebble/batch.go
+++ github.com/cockroachdb/pebble/batch.go
@@ -1195,8 +1195,19 @@ func (b *flushableBatch) newRangeDelIter(o *IterOptions) internalIterator {
 	return rangedel.NewIter(b.cmp, b.tombstones)
 }
 
-func (b *flushableBatch) totalBytes() uint64 {
+func (b *flushableBatch) inuseBytes() uint64 {
 	return uint64(len(b.data) - batchHeaderLen)
+}
+
+func (b *flushableBatch) totalBytes() uint64 {
+	return uint64(cap(b.data))
+}
+
+func (b *flushableBatch) readerRef() {
+	// TODO(peter): account for large batch memory in the cache.
+}
+
+func (b *flushableBatch) readerUnref() {
 }
 
 func (b *flushableBatch) flushed() chan struct{} {
