51544aebe7ad27d8fe0aebdbf92f860ed3d76669
Jordan Lewis <jordanthelewis@gmail.com>
2020-06-30 14:56:18 -0400

    add jordanlewis/gcassert
    This upgrades testify and go/analysis.

---
+++ github.com/jordanlewis/gcassert/README.md
@@ -0,0 +1,83 @@
+# gcassert
+
+gcassert is a program for making assertions about compiler decisions in
+Golang programs, via inline comment directives like `//gcassert:inline`.
+
+## Example
+
+Given a file `foo.go`:
+
+```
+package foo
+
+func addOne(i int) int {
+    return i+1
+}
+
+func a(ints []int) int {
+    var sum int
+    for i := range ints {
+        //gcassert:bce,inline
+        sum += addOne(ints[i])
+
+        sum += ints[i] //gcassert:bce
+    }
+    return sum
+}
+```
+
+The inline `//gcassert` directive will cause `gcassert` to fail if the line
+`sum += addOne(ints[i])` is either not inlined or contains bounds checks.
+
+`//gcassert` comments expect a comma-separated list of directives after
+`//gcassert:`. They can be included above the line in question or after, as an
+inline comment.
+
+## Installation
+
+```
+go get github.com/jordanlewis/gcassert/cmd/gcassert
+```
+
+## Usage
+
+Run gcassert on packages containing gcassert directives, like this:
+
+```
+gcassert ./package/path
+```
+
+The program will output all lines that had a gcassert directive that wasn't
+respected by the compiler.
+
+For example, running on the testdata directory in this library will produce the
+following output:
+
+```
+$ gcassert ./testdata
+testdata/bce.go:8:	fmt.Println(ints[5]): Found IsInBounds
+testdata/bce.go:16:	sum += notInlinable(ints[i]): call was not inlined
+testdata/inline.go:22:	sum += notInlinable(i): call was not inlined
+```
+
+Inspecting each of the listed lines will show a `//gcassert` directive
+that wasn't upheld when running the compiler on the package.
+
+## Directives
+
+
+```
+//gcassert:inline
+```
+
+The inline directive asserts that the following statement contains a function
+that is inlined by the compiler. If the function does not get inlined, gcassert
+will fail.
+
+```
+//gcassert:bce
+```
+
+The bce directive asserts that the following statement contains a slice index
+that has no necessary bounds checks. If the compiler adds bounds checks,
+gcassert will fail.
---
+++ github.com/jordanlewis/gcassert/gcassert.go
@@ -0,0 +1,246 @@
+// Copyright 2020 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package gcassert
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"go/ast"
+	"go/printer"
+	"go/token"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"golang.org/x/tools/go/packages"
+)
+
+type assertDirective int
+
+const (
+	noDirective assertDirective = iota
+	inline
+	bce
+)
+
+func stringToDirective(s string) (assertDirective, error) {
+	switch s {
+	case "inline":
+		return inline, nil
+	case "bce":
+		return bce, nil
+	}
+	return noDirective, errors.New(fmt.Sprintf("no such directive %s", s))
+}
+
+type lineInfo struct {
+	n          ast.Node
+	directives []assertDirective
+	// passedDirective is a map from index into the directives slice to a
+	// boolean that says whether or not the directive succeeded, in the case
+	// of directives like inlining that have compiler output if they passed.
+	// For directives like bce that have compiler output if they failed, there's
+	// no entry in this map.
+	passedDirective map[int]bool
+}
+
+var gcAssertRegex = regexp.MustCompile(`//gcassert:([\w,]+)`)
+
+type assertVisitor struct {
+	commentMap ast.CommentMap
+
+	directiveMap map[int]lineInfo
+	fileSet      *token.FileSet
+}
+
+func newAssertVisitor(commentMap ast.CommentMap, fileSet *token.FileSet) assertVisitor {
+	return assertVisitor{
+		commentMap:   commentMap,
+		fileSet:      fileSet,
+		directiveMap: make(map[int]lineInfo),
+	}
+}
+
+func (v assertVisitor) Visit(node ast.Node) (w ast.Visitor) {
+	if node == nil {
+		return w
+	}
+	m := v.commentMap[node]
+COMMENTLOOP:
+	for _, g := range m {
+		for _, c := range g.List {
+			matches := gcAssertRegex.FindStringSubmatch(c.Text)
+			if len(matches) == 0 {
+				continue COMMENTLOOP
+			}
+			// The 0th match is the whole string, and the 1st match is the
+			// gcassert directive(s).
+			directiveStrings := strings.Split(matches[1], ",")
+
+			pos := node.Pos()
+			lineNumber := v.fileSet.Position(pos).Line
+			lineInfo := v.directiveMap[lineNumber]
+			lineInfo.n = node
+			for _, s := range directiveStrings {
+				directive, err := stringToDirective(s)
+				if err != nil {
+					continue
+				}
+				lineInfo.directives = append(lineInfo.directives, directive)
+			}
+			v.directiveMap[lineNumber] = lineInfo
+		}
+	}
+	return v
+}
+
+// GCAssert searches through the packages at the input path and writes failures
+// to comply with //gcassert directives to the given io.Writer.
+func GCAssert(path string, w io.Writer) error {
+	fileSet := token.NewFileSet()
+	pkgs, err := packages.Load(&packages.Config{
+		Mode: packages.NeedName | packages.NeedFiles | packages.NeedSyntax | packages.NeedCompiledGoFiles,
+		Fset: fileSet,
+	}, path)
+	directiveMap, err := parseDirectives(pkgs, fileSet)
+	if err != nil {
+		return err
+	}
+
+	// Next: invoke Go compiler with -m flags to get the compiler to print
+	// its optimization decisions.
+
+	args := append([]string{"build", "-gcflags=-m -m -d=ssa/check_bce/debug=1"}, "./"+path)
+	cmd := exec.Command("go", args...)
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	cmd.Dir = cwd
+	pr, pw := io.Pipe()
+	cmd.Stdout = pw
+	cmd.Stderr = pw
+	cmdErr := make(chan error, 1)
+	go func() {
+		cmdErr <- cmd.Run()
+		pw.Close()
+	}()
+
+	scanner := bufio.NewScanner(pr)
+	optInfo := regexp.MustCompile(`([\.\/\w]+):(\d+):\d+: (.*)`)
+	boundsCheck := "Found IsInBounds"
+	sliceBoundsCheck := "Found SliceIsInBounds"
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := optInfo.FindStringSubmatch(line)
+		if len(matches) != 0 {
+			path := matches[1]
+			lineNo, err := strconv.Atoi(matches[2])
+			if err != nil {
+				return err
+			}
+			message := matches[3]
+
+			absPath, err := filepath.Abs(path)
+			if err != nil {
+				return err
+			}
+			if lineToDirectives := directiveMap[absPath]; lineToDirectives != nil {
+				info := lineToDirectives[lineNo]
+				if info.passedDirective == nil {
+					info.passedDirective = make(map[int]bool)
+					lineToDirectives[lineNo] = info
+				}
+				for i, d := range info.directives {
+					switch d {
+					case bce:
+						if message == boundsCheck || message == sliceBoundsCheck {
+							// Error! We found a bounds check where the user expected
+							// there to be none.
+							// Print out the user's code lineNo that failed the assertion,
+							// the assertion itself, and the compiler output that
+							// proved that the assertion failed.
+							if err := printAssertionFailure(cwd, fileSet, info, w, message); err != nil {
+								return err
+							}
+						}
+					case inline:
+						if strings.HasPrefix(message, "inlining call to") {
+							info.passedDirective[i] = true
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for _, lineToDirectives := range directiveMap {
+		for _, info := range lineToDirectives {
+			for i, d := range info.directives {
+				// An inlining directive passes if it has compiler output. For
+				// each inlining directive, check if there was matching compiler
+				// output and fail if not.
+				if d == inline {
+					if !info.passedDirective[i] {
+						if err := printAssertionFailure(
+							cwd, fileSet, info, w, "call was not inlined"); err != nil {
+							return err
+						}
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func printAssertionFailure(cwd string, fileSet *token.FileSet, info lineInfo, w io.Writer, message string) error {
+	var buf strings.Builder
+	_ = printer.Fprint(&buf, fileSet, info.n)
+	pos := fileSet.Position(info.n.Pos())
+	relPath, err := filepath.Rel(cwd, pos.Filename)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "%s:%d:\t%s: %s\n", relPath, pos.Line, buf.String(), message)
+	return nil
+}
+
+type directiveMap map[string]map[int]lineInfo
+
+func parseDirectives(pkgs []*packages.Package, fileSet *token.FileSet) (directiveMap, error) {
+	fileDirectiveMap := make(directiveMap)
+	for _, pkg := range pkgs {
+		for i, file := range pkg.Syntax {
+			commentMap := ast.NewCommentMap(fileSet, file, file.Comments)
+
+			v := newAssertVisitor(commentMap, fileSet)
+			// First: find all lines of code annotated with our gcassert directives.
+			ast.Walk(v, file)
+
+			file := pkg.CompiledGoFiles[i]
+			idx := strings.LastIndex(file, pkg.PkgPath)
+			if idx == -1 {
+				return nil, fmt.Errorf("couldn't find file %s in package %s", file, pkg.PkgPath)
+			}
+			if len(v.directiveMap) > 0 {
+				fileDirectiveMap[file] = v.directiveMap
+			}
+		}
+	}
+	return fileDirectiveMap, nil
+}
