1b1d687f4f28f4d6aac6ec8645cbc22e28c0b183
Raphael 'kena' Poss <knz@thaumogen.net>
2020-08-22 09:28:51 +0200

    bump cockroachdb/errors

--- github.com/cockroachdb/errors/errutil/message.go
+++ github.com/cockroachdb/errors/errutil/message.go
@@ -14,13 +14,7 @@
 
 package errutil
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/cockroachdb/errors/errbase"
-	"github.com/gogo/protobuf/proto"
-)
+import "github.com/cockroachdb/redact"
 
 // WithMessage annotates err with a new message.
 // If err is nil, WithMessage returns nil.
@@ -28,9 +22,9 @@ func WithMessage(err error, message string) error {
 	if err == nil {
 		return nil
 	}
-	return &withMessage{
-		cause: err,
-		msg:   message,
+	return &withPrefix{
+		cause:  err,
+		prefix: redact.Sprint(redact.Safe(message)),
 	}
 }
 
@@ -40,35 +34,8 @@ func WithMessagef(err error, format string, args ...interface{}) error {
 	if err == nil {
 		return nil
 	}
-	return &withMessage{
-		cause: err,
-		msg:   fmt.Sprintf(format, args...),
+	return &withPrefix{
+		cause:  err,
+		prefix: redact.Sprintf(format, args...),
 	}
-}
-
-type withMessage struct {
-	cause error
-	msg   string
-}
-
-func (w *withMessage) Error() string { return fmt.Sprintf("%s: %v", w.msg, w.cause) }
-func (w *withMessage) Cause() error  { return w.cause }
-func (w *withMessage) Unwrap() error { return w.cause }
-
-func (w *withMessage) Format(s fmt.State, verb rune) { errbase.FormatError(w, s, verb) }
-func (w *withMessage) FormatError(p errbase.Printer) (next error) {
-	p.Print(w.msg)
-	return w.cause
-}
-
-// A message wrapper is decoded exactly.
-func decodeMessage(
-	ctx context.Context, cause error, msg string, _ []string, _ proto.Message,
-) error {
-	return &withMessage{cause: cause, msg: msg}
-}
-
-func init() {
-	tn := errbase.GetTypeKey((*withMessage)(nil))
-	errbase.RegisterWrapperDecoder(tn, decodeMessage)
 }
