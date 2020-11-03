e74f220be7715c9350aa108ce05108b9ad2c9d0a
Tobias Grieger <tobias.b.grieger@gmail.com>
2020-11-03 11:17:27 +0100

    Bump raft

--- honnef.co/go/tools/lint/runner.go
+++ honnef.co/go/tools/lint/runner.go
@@ -59,18 +59,21 @@ const sanityCheck = true
 // interfaces from transitive dependencies.
 
 type Package struct {
+	dependents uint64
+
 	*packages.Package
-	Imports    map[string]*Package
+	Imports    []*Package
 	initial    bool
 	fromSource bool
 	hash       string
 	done       chan struct{}
 
 	resultsMu sync.Mutex
-	results   []*result
+	// results maps analyzer IDs to analyzer results
+	results []*result
 
 	cfg      *config.Config
-	gen      map[string]bool
+	gen      map[string]facts.Generator
 	problems []Problem
 	ignores  []Ignore
 	errs     []error
@@ -78,6 +81,24 @@ type Package struct {
 	// these slices are indexed by analysis
 	facts    []map[types.Object][]analysis.Fact
 	pkgFacts [][]analysis.Fact
+
+	canClearTypes bool
+}
+
+func (pkg *Package) decUse() {
+	atomic.AddUint64(&pkg.dependents, ^uint64(0))
+	if atomic.LoadUint64(&pkg.dependents) == 0 {
+		// nobody depends on this package anymore
+		if pkg.canClearTypes {
+			pkg.Types = nil
+		}
+		pkg.facts = nil
+		pkg.pkgFacts = nil
+
+		for _, imp := range pkg.Imports {
+			imp.decUse()
+		}
+	}
 }
 
 type result struct {
@@ -95,7 +116,8 @@ type Runner struct {
 	// limits parallelism of loading packages
 	loadSem chan struct{}
 
-	stats *Stats
+	goVersion int
+	stats     *Stats
 }
 
 type analyzerIDs struct {
@@ -115,19 +137,18 @@ type Fact struct {
 	Fact analysis.Fact
 }
 
-type newFact struct {
-	obj  types.Object
-	fact analysis.Fact
-}
-
 type analysisAction struct {
-	analyzer   *analysis.Analyzer
-	analyzerID int
-	pkg        *Package
-	newFacts   []newFact
-	problems   []Problem
+	analyzer        *analysis.Analyzer
+	analyzerID      int
+	pkg             *Package
+	newPackageFacts []analysis.Fact
+	problems        []Problem
 
 	pkgFacts map[*types.Package][]analysis.Fact
+}
+
+func (ac *analysisAction) String() string {
+	return fmt.Sprintf("%s @ %s", ac.analyzer, ac.pkg)
 }
 
 func (ac *analysisAction) allObjectFacts() []analysis.ObjectFact {
@@ -194,12 +215,13 @@ func (ac *analysisAction) exportPackageFact(fact analysis.Fact) {
 		panic("analysis doesn't export any facts")
 	}
 	ac.pkgFacts[ac.pkg.Types] = append(ac.pkgFacts[ac.pkg.Types], fact)
-	ac.newFacts = append(ac.newFacts, newFact{nil, fact})
+	ac.newPackageFacts = append(ac.newPackageFacts, fact)
 }
 
 func (ac *analysisAction) report(pass *analysis.Pass, d analysis.Diagnostic) {
 	p := Problem{
 		Pos:     DisplayPosition(pass.Fset, d.Pos),
+		End:     DisplayPosition(pass.Fset, d.End),
 		Message: d.Message,
 		Check:   pass.Analyzer.Name,
 	}
@@ -225,11 +247,6 @@ func (r *Runner) runAnalysis(ac *analysisAction) (ret interface{}, err error) {
 			res.err = err
 			close(res.ready)
 		}()
-
-		// Package may be a dependency or a package the user requested
-		// Facts for a dependency may be cached or not
-		// Diagnostics for a user package may be cached or not (not yet)
-		// When we have to analyze a package, we have to analyze it with all dependencies.
 
 		pass := new(analysis.Pass)
 		*pass = analysis.Pass{
@@ -337,6 +354,9 @@ func (r *Runner) makeAnalysisAction(a *analysis.Analyzer, pkg *Package) *analysi
 	return ac
 }
 
+// analyzes that we always want to run, even if they're not being run
+// explicitly or as dependencies. these are necessary for the inner
+// workings of the runner.
 var injectedAnalyses = []*analysis.Analyzer{facts.Generated, config.Analyzer}
 
 func (r *Runner) runAnalysisUser(pass *analysis.Pass, ac *analysisAction) (interface{}, error) {
@@ -346,16 +366,7 @@ func (r *Runner) runAnalysisUser(pass *analysis.Pass, ac *analysisAction) (inter
 
 	// User-provided package, analyse it
 	// First analyze it with dependencies
-	var req []*analysis.Analyzer
-	req = append(req, ac.analyzer.Requires...)
-	if pass.Analyzer != facts.Generated && pass.Analyzer != config.Analyzer {
-		// Ensure all packages have the generated map and config. This is
-		// required by interna of the runner. Analyses that themselves
-		// make use of either have an explicit dependency so that other
-		// runners work correctly, too.
-		req = append(req, injectedAnalyses...)
-	}
-	for _, req := range req {
+	for _, req := range ac.analyzer.Requires {
 		acReq := r.makeAnalysisAction(req, ac.pkg)
 		ret, err := r.runAnalysis(acReq)
 		if err != nil {
@@ -373,24 +384,12 @@ func (r *Runner) runAnalysisUser(pass *analysis.Pass, ac *analysisAction) (inter
 	}
 
 	if len(ac.analyzer.FactTypes) > 0 {
-		// Merge new facts into the package.
-		for _, fact := range ac.newFacts {
-			if fact.obj == nil {
-				id := r.analyzerIDs.get(ac.analyzer)
-				ac.pkg.pkgFacts[id] = append(ac.pkg.pkgFacts[id], fact.fact)
-			} else {
-				panic("unexpected new object fact")
-			}
-		}
-
-		// Persist facts to cache
+		// Merge new facts into the package and persist them.
 		var facts []Fact
-		for _, fact := range ac.newFacts {
-			if fact.obj == nil {
-				facts = append(facts, Fact{"", fact.fact})
-			} else {
-				panic("unexpected object fact")
-			}
+		for _, fact := range ac.newPackageFacts {
+			id := r.analyzerIDs.get(ac.analyzer)
+			ac.pkg.pkgFacts[id] = append(ac.pkg.pkgFacts[id], fact)
+			facts = append(facts, Fact{"", fact})
 		}
 		for obj, afacts := range ac.pkg.facts[ac.analyzerID] {
 			if obj.Pkg() != ac.pkg.Package.Types {
@@ -434,7 +433,14 @@ func NewRunner(stats *Stats) (*Runner, error) {
 	}, nil
 }
 
-func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analysis.Analyzer) ([]*Package, error) {
+// Run loads packages corresponding to patterns and analyses them with
+// analyzers. It returns the loaded packages, which contain reported
+// diagnostics as well as extracted ignore directives.
+//
+// Note that diagnostics have not been filtered at this point yet, to
+// accomodate cumulative analyzes that require additional steps to
+// produce diagnostics.
+func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analysis.Analyzer, hasCumulative bool) ([]*Package, error) {
 	r.analyzerIDs = analyzerIDs{m: map[*analysis.Analyzer]int{}}
 	id := 0
 	seen := map[*analysis.Analyzer]struct{}{}
@@ -454,6 +460,9 @@ func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analy
 		}
 	}
 	for _, a := range analyzers {
+		if v := a.Flags.Lookup("go"); v != nil {
+			v.Value.Set(fmt.Sprintf("1.%d", r.goVersion))
+		}
 		dfs(a)
 	}
 	for _, a := range injectedAnalyses {
@@ -465,7 +474,7 @@ func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analy
 		dcfg = *cfg
 	}
 
-	atomic.StoreUint64(&r.stats.State, StateGraph)
+	atomic.StoreUint32(&r.stats.State, StateGraph)
 	initialPkgs, err := r.ld.Graph(dcfg, patterns...)
 	if err != nil {
 		return nil, err
@@ -478,11 +487,13 @@ func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analy
 	packages.Visit(initialPkgs, nil, func(l *packages.Package) {
 		m[l] = &Package{
 			Package:  l,
-			Imports:  map[string]*Package{},
 			results:  make([]*result, len(r.analyzerIDs.m)),
 			facts:    make([]map[types.Object][]analysis.Fact, len(r.analyzerIDs.m)),
 			pkgFacts: make([][]analysis.Fact, len(r.analyzerIDs.m)),
 			done:     make(chan struct{}),
+			// every package needs itself
+			dependents:    1,
+			canClearTypes: !hasCumulative,
 		}
 		allPkgs = append(allPkgs, m[l])
 		for i := range m[l].facts {
@@ -491,8 +502,9 @@ func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analy
 		for _, err := range l.Errors {
 			m[l].errs = append(m[l].errs, err)
 		}
-		for k, v := range l.Imports {
-			m[l].Imports[k] = m[v]
+		for _, v := range l.Imports {
+			m[v].dependents++
+			m[l].Imports = append(m[l].Imports, m[v])
 		}
 
 		m[l].hash, err = packageHash(m[l])
@@ -500,29 +512,30 @@ func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analy
 			m[l].errs = append(m[l].errs, err)
 		}
 	})
+
 	pkgs := make([]*Package, len(initialPkgs))
 	for i, l := range initialPkgs {
 		pkgs[i] = m[l]
 		pkgs[i].initial = true
 	}
 
-	atomic.StoreUint64(&r.stats.InitialPackages, uint64(len(initialPkgs)))
-	atomic.StoreUint64(&r.stats.TotalPackages, uint64(len(allPkgs)))
-	atomic.StoreUint64(&r.stats.State, StateProcessing)
+	atomic.StoreUint32(&r.stats.InitialPackages, uint32(len(initialPkgs)))
+	atomic.StoreUint32(&r.stats.TotalPackages, uint32(len(allPkgs)))
+	atomic.StoreUint32(&r.stats.State, StateProcessing)
 
 	var wg sync.WaitGroup
 	wg.Add(len(allPkgs))
 	r.loadSem = make(chan struct{}, runtime.GOMAXPROCS(-1))
-	atomic.StoreUint64(&r.stats.TotalWorkers, uint64(cap(r.loadSem)))
+	atomic.StoreUint32(&r.stats.TotalWorkers, uint32(cap(r.loadSem)))
 	for _, pkg := range allPkgs {
 		pkg := pkg
 		go func() {
 			r.processPkg(pkg, analyzers)
 
 			if pkg.initial {
-				atomic.AddUint64(&r.stats.ProcessedInitialPackages, 1)
+				atomic.AddUint32(&r.stats.ProcessedInitialPackages, 1)
 			}
-			atomic.AddUint64(&r.stats.Problems, uint64(len(pkg.problems)))
+			atomic.AddUint32(&r.stats.Problems, uint32(len(pkg.problems)))
 			wg.Done()
 		}()
 	}
@@ -531,15 +544,15 @@ func (r *Runner) Run(cfg *packages.Config, patterns []string, analyzers []*analy
 	return pkgs, nil
 }
 
-var posRe = regexp.MustCompile(`^(.+?):(\d+)(?::(\d+)?)?$`)
+var posRe = regexp.MustCompile(`^(.+?):(\d+)(?::(\d+)?)?`)
 
-func parsePos(pos string) token.Position {
+func parsePos(pos string) (token.Position, int, error) {
 	if pos == "-" || pos == "" {
-		return token.Position{}
+		return token.Position{}, 0, nil
 	}
 	parts := posRe.FindStringSubmatch(pos)
 	if parts == nil {
-		panic(fmt.Sprintf("internal error: malformed position %q", pos))
+		return token.Position{}, 0, fmt.Errorf("malformed position %q", pos)
 	}
 	file := parts[1]
 	line, _ := strconv.Atoi(parts[2])
@@ -548,7 +561,7 @@ func parsePos(pos string) token.Position {
 		Filename: file,
 		Line:     line,
 		Column:   col,
-	}
+	}, len(parts[0]), nil
 }
 
 // loadPkg loads a Go package. If the package is in the set of initial
@@ -676,9 +689,16 @@ func (r *Runner) processPkg(pkg *Package, analyzers []*analysis.Analyzer) {
 		pkg.Syntax = nil
 		pkg.results = nil
 
-		atomic.AddUint64(&r.stats.ProcessedPackages, 1)
+		atomic.AddUint32(&r.stats.ProcessedPackages, 1)
+		pkg.decUse()
 		close(pkg.done)
 	}()
+
+	// Ensure all packages have the generated map and config. This is
+	// required by interna of the runner. Analyses that themselves
+	// make use of either have an explicit dependency so that other
+	// runners work correctly, too.
+	analyzers = append(analyzers[0:len(analyzers):len(analyzers)], injectedAnalyses...)
 
 	if len(pkg.errs) != 0 {
 		return
@@ -688,6 +708,9 @@ func (r *Runner) processPkg(pkg *Package, analyzers []*analysis.Analyzer) {
 		<-imp.done
 		if len(imp.errs) > 0 {
 			if imp.initial {
+				// Don't print the error of the dependency since it's
+				// an initial package and we're already printing the
+				// error.
 				pkg.errs = append(pkg.errs, fmt.Errorf("could not analyze dependency %s of %s", imp, pkg))
 			} else {
 				var s string
@@ -705,10 +728,10 @@ func (r *Runner) processPkg(pkg *Package, analyzers []*analysis.Analyzer) {
 	}
 
 	r.loadSem <- struct{}{}
-	atomic.AddUint64(&r.stats.ActiveWorkers, 1)
+	atomic.AddUint32(&r.stats.ActiveWorkers, 1)
 	defer func() {
 		<-r.loadSem
-		atomic.AddUint64(&r.stats.ActiveWorkers, ^uint64(0))
+		atomic.AddUint32(&r.stats.ActiveWorkers, ^uint32(0))
 	}()
 	if err := r.loadPkg(pkg, analyzers); err != nil {
 		pkg.errs = append(pkg.errs, err)
@@ -783,10 +806,15 @@ func (r *Runner) processPkg(pkg *Package, analyzers []*analysis.Analyzer) {
 	for _, ac := range acs {
 		pkg.problems = append(pkg.problems, ac.problems...)
 	}
-	if pkg.results[r.analyzerIDs.get(config.Analyzer)].v != nil {
-		pkg.cfg = pkg.results[r.analyzerIDs.get(config.Analyzer)].v.(*config.Config)
+
+	if pkg.initial {
+		// Only initial packages have these analyzers run, and only
+		// initial packages need these.
+		if pkg.results[r.analyzerIDs.get(config.Analyzer)].v != nil {
+			pkg.cfg = pkg.results[r.analyzerIDs.get(config.Analyzer)].v.(*config.Config)
+		}
+		pkg.gen = pkg.results[r.analyzerIDs.get(facts.Generated)].v.(map[string]facts.Generator)
 	}
-	pkg.gen = pkg.results[r.analyzerIDs.get(facts.Generated)].v.(map[string]bool)
 
 	// In a previous version of the code, we would throw away all type
 	// information and reload it from export data. That was
@@ -796,6 +824,9 @@ func (r *Runner) processPkg(pkg *Package, analyzers []*analysis.Analyzer) {
 	// from processPkg.
 }
 
+// hasFacts reports whether an analysis exports any facts. An analysis
+// that has a transitive dependency that exports facts is considered
+// to be exporting facts.
 func (r *Runner) hasFacts(a *analysis.Analyzer) bool {
 	ret := false
 	seen := make([]bool, len(r.analyzerIDs.m))
@@ -828,6 +859,8 @@ func parseDirective(s string) (cmd string, args []string) {
 	return fields[0], fields[1:]
 }
 
+// parseDirectives extracts all linter directives from the source
+// files of the package. Malformed directives are returned as problems.
 func parseDirectives(pkg *packages.Package) ([]Ignore, []Problem) {
 	var ignores []Ignore
 	var problems []Problem
@@ -857,12 +890,11 @@ func parseDirectives(pkg *packages.Package) ([]Ignore, []Problem) {
 					switch cmd {
 					case "ignore", "file-ignore":
 						if len(args) < 2 {
-							// FIXME(dh): this causes duplicated warnings when using megacheck
 							p := Problem{
 								Pos:      DisplayPosition(pkg.Fset, c.Pos()),
 								Message:  "malformed linter directive; missing the required reason field?",
 								Severity: Error,
-								Check:    "",
+								Check:    "compile",
 							}
 							problems = append(problems, p)
 							continue
@@ -897,6 +929,9 @@ func parseDirectives(pkg *packages.Package) ([]Ignore, []Problem) {
 	return ignores, problems
 }
 
+// packageHash computes a package's hash. The hash is based on all Go
+// files that make up the package, as well as the hashes of imported
+// packages.
 func packageHash(pkg *Package) (string, error) {
 	key := cache.NewHash("package hash")
 	fmt.Fprintf(key, "pkgpath %s\n", pkg.PkgPath)
@@ -907,10 +942,9 @@ func packageHash(pkg *Package) (string, error) {
 		}
 		fmt.Fprintf(key, "file %s %x\n", f, h)
 	}
-	imps := make([]*Package, 0, len(pkg.Imports))
-	for _, v := range pkg.Imports {
-		imps = append(imps, v)
-	}
+
+	imps := make([]*Package, len(pkg.Imports))
+	copy(imps, pkg.Imports)
 	sort.Slice(imps, func(i, j int) bool {
 		return imps[i].PkgPath < imps[j].PkgPath
 	})
@@ -925,6 +959,7 @@ func packageHash(pkg *Package) (string, error) {
 	return hex.EncodeToString(h[:]), nil
 }
 
+// passActionID computes an ActionID for an analysis pass.
 func passActionID(pkg *Package, analyzer *analysis.Analyzer) (cache.ActionID, error) {
 	key := cache.NewHash("action ID")
 	fmt.Fprintf(key, "pkgpath %s\n", pkg.PkgPath)
