88451783657a8e7c399cc3ae217f4f763d0ac707
Rafi Shamim <rafi@cockroachlabs.com>
2022-01-24 12:02:44 -0500

    Update to cockroach-go 2.2.6

--- github.com/cockroachdb/cockroach-go/v2/crdb/error.go
+++ github.com/cockroachdb/cockroach-go/v2/crdb/error.go
@@ -1,3 +1,17 @@
+// Copyright 2016 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
 package crdb
 
 import "fmt"
@@ -56,6 +70,9 @@ func newMaxRetriesExceededError(err error, maxRetries int) *MaxRetriesExceededEr
 		msg:     fmt.Sprintf(msgPattern, maxRetries, err),
 	}
 }
+
+// Error implements the error interface.
+func (e *MaxRetriesExceededError) Error() string { return e.msg }
 
 // TxnRestartError represents an error when restarting a transaction. `cause` is
 // the error from restarting the txn and `retryCause` is the original error which
