d788a3fadaebef1f6471e9974206697e4f86977b
Rui Hu <rui@cockroachlabs.com>
2022-05-27 15:51:35 -0400

    downgrade grpc to v1.46.0

--- google.golang.org/grpc/clientconn.go
+++ google.golang.org/grpc/clientconn.go
@@ -907,10 +907,14 @@ func (cc *ClientConn) healthCheckConfig() *healthCheckConfig {
 }
 
 func (cc *ClientConn) getTransport(ctx context.Context, failfast bool, method string) (transport.ClientTransport, func(balancer.DoneInfo), error) {
-	return cc.blockingpicker.pick(ctx, failfast, balancer.PickInfo{
+	t, done, err := cc.blockingpicker.pick(ctx, failfast, balancer.PickInfo{
 		Ctx:            ctx,
 		FullMethodName: method,
 	})
+	if err != nil {
+		return nil, nil, toRPCErr(err)
+	}
+	return t, done, nil
 }
 
 func (cc *ClientConn) applyServiceConfigAndBalancer(sc *ServiceConfig, configSelector iresolver.ConfigSelector, addrs []resolver.Address) {
